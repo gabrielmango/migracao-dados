1da0db1660c5331ba50220dc02d55d3944defb18
Gabriel Mango <gabrielhmango@gmail.com>
2023-09-01 11:08:55 -0300

    Carregamento dos dados no Banco de Dados

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 from database import DataBase
-from consistencia import Consistencia
+from consistencia import Consistencia, InsertPessoa
 
 dados = pd.read_csv('Migração.csv')
 print('Dados carregados da planilha.')
 
 dados_list = []
+pessoas_nao_cadastradas = []
+
+couuid_1 = 'python-gabriel'
 
 consistencia = Consistencia()
 database = DataBase()
@@ -43,3 +46,23 @@ for number in range(len(dados)):
     dados_list.append(dado_formatado)
     
 print('Dados preprocessados.')
+
+# Pessoas com CPF e com RG
+for dado in dados_list:
+    if dado['CPF'] and len(dado['CPF']) == 10:
+        dado['CPF'] = '0' + dado['CPF']
+
+for dado in dados_list:
+    pessoa = InsertPessoa(couuid_1, dado)
+    
+    if pessoa.pessoa_na_base():
+        print(f'{dado["NOME"]} já se encontra na base de dados')
+        pessoas_nao_cadastradas.append(dado)
+
+    if dado['CPF'] and (len(dado['CPF']) < 11 or len(dado['CPF']) > 12):
+        dado['CPF'] = None
+    
+    pessoa.executa_insert()
+
+print('Dados carregados no banco de dados.')
+print(pessoas_nao_cadastradas)
